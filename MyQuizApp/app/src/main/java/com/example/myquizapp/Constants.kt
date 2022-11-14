177a0bbf9208f6f1fe4986af7c663f2846d05126
ekdls <ekdls278@gmail.com>
2022-11-14 23:23:01 +0900

    MyQuizApp

---
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.myquizapp
+
+object Constants {
+
+}
---
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.myquizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_questions);
+    }
+}
